1a3bf02a517f10bbee10434f0b808b20f278a327
LuisCervantes6368 <ba441259@uaeh.edu.mx>
2024-05-13 15:59:52 -0600

    Se agrega el proyecto

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    private GameObject Ball;
+    private int contador = 0;
+    private void Start()
+    {
+        Ball = GameObject.FindWithTag("Ball");
+    }
+   
+        void OnGUI()
+    {
+        
+        if (Ball != null && Ball.transform.position.z <= -6.9f)
+        {
+            if (GUI.Button(new Rect(5, 5, 100, 20), "Restart"))
+            {
+                contador++;
+                if (contador >= 3)
+                {
+                    SceneManager.LoadScene("Assets/Scenes/scene2.unity", LoadSceneMode.Single);
+                }
+                else
+                {
+                    SceneManager.LoadScene("Assets/Scenes/scene1.unity", LoadSceneMode.Single);
+                }
+            }
+        }
+    }
+}
